a66ec3a7a43ffe539d0eec1cd42c5cbbc417c466
bacancy-aashka <aashka.bhatt@bacancytechnology.com>
2020-03-31 12:24:03 +0530

    "cHANGE"

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,18 @@
 
 ActiveRecord::Schema.define(version: 2020_03_27_132159) do
 
+  create_table "bills", force: :cascade do |t|
+    t.integer "request_id", null: false
+    t.string "method"
+    t.float "tax"
+    t.float "amount"
+    t.float "provider_charge"
+    t.float "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["request_id"], name: "index_bills_on_request_id"
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -21,7 +33,7 @@ ActiveRecord::Schema.define(version: 2020_03_27_132159) do
   create_table "provider_details", force: :cascade do |t|
     t.string "city"
     t.string "state"
-    t.string "zipcode"
+    t.integer "zipcode"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "provider_id"
@@ -31,11 +43,65 @@ ActiveRecord::Schema.define(version: 2020_03_27_132159) do
     t.index ["provider_id"], name: "index_provider_details_on_provider_id"
   end
 
+  create_table "provider_requests", force: :cascade do |t|
+    t.string "status"
+    t.integer "provider_id", null: false
+    t.integer "request_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["provider_id"], name: "index_provider_requests_on_provider_id"
+    t.index ["request_id"], name: "index_provider_requests_on_request_id"
+  end
+
+  create_table "provider_skills", force: :cascade do |t|
+    t.integer "experience"
+    t.integer "charges"
+    t.boolean "avail"
+    t.integer "skill_id", null: false
+    t.integer "provider_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["provider_id"], name: "index_provider_skills_on_provider_id"
+    t.index ["skill_id"], name: "index_provider_skills_on_skill_id"
+  end
+
   create_table "providers", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_providers_on_user_id"
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.datetime "request_from"
+    t.datetime "requesst_to"
+    t.integer "skill_id", null: false
+    t.integer "user_id", null: false
+    t.integer "provider_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["provider_id"], name: "index_requests_on_provider_id"
+    t.index ["skill_id"], name: "index_requests_on_skill_id"
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.integer "resource_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "skill_name"
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_skills_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,7 +120,24 @@ ActiveRecord::Schema.define(version: 2020_03_27_132159) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
+  end
+
+  add_foreign_key "bills", "requests"
   add_foreign_key "provider_details", "categories"
   add_foreign_key "provider_details", "providers"
+  add_foreign_key "provider_requests", "providers"
+  add_foreign_key "provider_requests", "requests"
+  add_foreign_key "provider_skills", "providers"
+  add_foreign_key "provider_skills", "skills"
   add_foreign_key "providers", "users"
+  add_foreign_key "requests", "providers"
+  add_foreign_key "requests", "skills"
+  add_foreign_key "requests", "users"
+  add_foreign_key "skills", "categories"
 end
