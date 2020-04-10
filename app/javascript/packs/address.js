200445e45ee2f9f78a3014d542052745fbc1f493
bacancy-aashka <aashka.bhatt@bacancytechnology.com>
2020-04-10 11:24:24 +0530

    "Javascript File Chaged For Searching"

--- app/javascript/packs/address.js
+++ app/javascript/packs/address.js
@@ -27,5 +27,16 @@ document.addEventListener("turbolinks:load", function(){
     placeholder: 'Select City',
     theme: "bootstrap",
     width: '150px'
-  }).val('').trigger('change');
+  }).on("change",function () {
+    $.ajax({
+        type: "GET",
+        url: "/home/filter_for_cities",
+        data: {city:  $("#city").val() },
+        ContentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data){
+            document.getElementById("provider-detail").innerHTML=data.provider_detail;
+        }
+    });
+  });
 })
