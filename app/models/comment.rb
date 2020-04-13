b7a19d50de991dfafc2b048781617e42f3cb73d5
bacancy-aashka <aashka.bhatt@bacancytechnology.com>
2020-04-13 19:36:26 +0530

    "Added Rating Feature For Provider"

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :provider_detail
   belongs_to :user
+
+  def self.get_average
+    (self.average(:rating)) != nil ? (self.average(:rating)).round(1) : 0
+  end
 end
