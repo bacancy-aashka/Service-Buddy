853ef5e57d09927ea169c14357649373327ab93f
bacancy-bhargavthummar <bhargav.thummar@bacancytechnology.com>
2020-04-14 15:20:21 +0530

    Validations added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :provider_detail
   belongs_to :user
 
+# VALIDATION :
+# validates :body, length: { maximum: 150, minimum: 1, message: 'must be present.' }
+
   def self.get_average
     (self.average(:rating)) != nil ? (self.average(:rating)).round(1) : 0
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,33 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :image
+  has_one_attached :image, dependent: :destroy
   has_one :provider, dependent: :destroy
   has_many :messages
   has_many :conversations, foreign_key: :sender_id
   has_many :comments, dependent: :destroy
 
+  # VALIDATION :
+  # validates :firstname, :lastname, presence: true
+  # validate :birth_date_future?
+  # validates :contact, :presence => { :message => 'is not valid' },
+  #                     :numericality => true,
+  #                     :length => { :minimum => 10, :maximum => 15 }
+
   def fullname
     "#{firstname.capitalize} #{lastname.capitalize}"
   end
+
+  # private
+
+  # def birth_date_future?
+  #   if dob.present?
+  #     if dob > Date.today
+  #       errors.add(:dob, "can't be in the future")
+  #     end
+  #   else
+  #     errors.add(:dob, "can't be blank")
+  #   end  
+  # end
+
 end
