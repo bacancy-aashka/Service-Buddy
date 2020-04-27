2b69c47c1f1a764db1fbc66d8d82931cac71c839
bacancy-aashka <aashka.bhatt@bacancytechnology.com>
2020-04-27 17:44:01 +0530

    "CSS Changed"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_100937) do
+ActiveRecord::Schema.define(version: 2020_04_24_081638) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
