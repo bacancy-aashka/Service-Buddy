bac96f41f0eddbf001e4879e35b033c1526f5d5d
bacancy-aashka <aashka.bhatt@bacancytechnology.com>
2020-05-04 14:38:51 +0530

    "Production File Changed"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,20 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Service_Buddy_production"
 
   config.action_mailer.perform_caching = false
+  config.require_master_key = true
+
+
+  config.action_mailer.default_url_options = { host: 'mighty-shelf-04048.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: 'plain',
+    user_name: ENV['EMAIL'],
+    password: ENV['PASSWORD'],
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
